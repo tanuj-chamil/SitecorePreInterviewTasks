e17b4a9415e34cb7c1470db1b825c37727129779
tan0s <tanujchamil@gmail.com>
2024-08-14 19:10:08 -0700

    Working Line Class

---
+++ SitecorePreInterviewTasks.UnitTests/Task01/LineTests.cs
@@ -0,0 +1,84 @@
+﻿using SitecorePreInterviewTasks.Task01;
+
+namespace SitecorePreInterviewTasks.UnitTests.Task01
+{
+    [TestClass]
+    public class LineTests
+    {
+        [TestMethod]
+        public void Move_ShouldMoveLineByGivenCoordinates()
+        {
+            // Arrange
+            var start = new Point(1, 1);
+            var end = new Point(2, 2);
+            var line = new Line(start, end);
+
+            // Act
+            line.Move(2, 3);
+
+            // Assert
+            Assert.AreEqual(3, line.Start.X);
+            Assert.AreEqual(4, line.Start.Y);
+            Assert.AreEqual(4, line.End.X);
+            Assert.AreEqual(5, line.End.Y);
+        }
+
+        [TestMethod]
+        public void Move_ShouldMoveLineByGivenPoint()
+        {
+            // Arrange
+            var start = new Point(1, 1);
+            var end = new Point(2, 2);
+            var line = new Line(start, end);
+            var movePoint = new Point(2, 3);
+
+            // Act
+            line.Move(movePoint);
+
+            // Assert
+            Assert.AreEqual(3, line.Start.X);
+            Assert.AreEqual(4, line.Start.Y);
+            Assert.AreEqual(4, line.End.X);
+            Assert.AreEqual(5, line.End.Y);
+        }
+
+        [TestMethod]
+        public void Rotate_ShouldRotateLineByGivenAngle()
+        {
+            // Arrange
+            var start = new Point(1, 1);
+            var end = new Point(2, 2);
+            var line = new Line(start, end);
+
+            // Act
+            line.Rotate(Math.PI / 2);
+
+            // Assert
+            Assert.AreEqual(-1, line.Start.X, 0.0001);
+            Assert.AreEqual(1, line.Start.Y, 0.0001);
+            Assert.AreEqual(-2, line.End.X, 0.0001);
+            Assert.AreEqual(2, line.End.Y, 0.0001);
+        }
+
+
+        [TestMethod]
+        public void Rotate_ShouldRotateLineByGivenAngle_DoublePrecision()
+        {
+            // Arrange
+            var start = new Point(1.0, 1.0);
+            var end = new Point(2.0, 2.0);
+            var line = new Line(start, end);
+
+            // Act
+            line.Rotate(Math.PI / 4);
+
+            // Assert
+            Assert.AreEqual(0.0, line.Start.X, 0.001);
+            Assert.AreEqual(1.414, line.Start.Y, 0.001);
+            Assert.AreEqual(0, line.End.X, 0.001);
+            Assert.AreEqual(2.828, line.End.Y, 0.001);
+        }
+
+
+    }
+}
---
+++ SitecorePreInterviewTasks/Task01/Line.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitecorePreInterviewTasks.Task01
+{
+    /// <summary>
+    /// Represents a line in 2D space.
+    /// </summary>
+    public class Line
+    {
+        public Point Start { get; set; }
+        public Point End { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Line"/> class.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        public Line(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        /// <summary>
+        /// Moves the line by the given coordinates.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void Move(double x, double y)
+        {
+            Start.Move(x, y);
+            End.Move(x, y);
+        }
+
+
+        /// <summary>
+        /// Moves the line by the given point.
+        /// </summary>
+        /// <param name="point"></param>
+        public void Move(Point point)
+        {
+            Start.Move(point);
+            End.Move(point);
+        }
+
+        /// <summary>
+        /// Rotates the line by the given angle.
+        /// </summary>
+        /// <param name="angle"></param>
+        public void Rotate(double angle)
+        {
+            Start.Rotate(angle);
+            End.Rotate(angle);
+        }
+
+
+    }
+}
