ed6fde6c4068e5b0ec3e48e30c6269b19fcfa02a
tan0s <tanujchamil@gmail.com>
2024-08-14 19:16:44 -0700

    Working Circle Class

---
+++ SitecorePreInterviewTasks.UnitTests/Task01/CircleTests.cs
@@ -0,0 +1,66 @@
+﻿using SitecorePreInterviewTasks.Task01;
+
+namespace SitecorePreInterviewTasks.UnitTests.Task01
+{
+    [TestClass]
+    public class CircleTests
+    {
+        [TestMethod]
+        public void Move_ShouldMoveCircleByGivenCoordinates()
+        {
+            // Arrange
+            var circle = new SitecorePreInterviewTasks.Task01.Circle(new Point(1, 1), 1);
+
+            // Act
+            circle.Move(2, 3);
+
+            // Assert
+            Assert.AreEqual(3, circle.Center.X);
+            Assert.AreEqual(4, circle.Center.Y);
+        }
+
+        [TestMethod]
+        public void Move_ShouldMoveCircleByGivenPoint()
+        {
+            // Arrange
+            var circle = new SitecorePreInterviewTasks.Task01.Circle(new Point(1, 1), 1);
+            var movePoint = new Point(2, 3);
+
+            // Act
+            circle.Move(movePoint);
+
+            // Assert
+            Assert.AreEqual(3, circle.Center.X);
+            Assert.AreEqual(4, circle.Center.Y);
+        }
+
+        [TestMethod]
+        public void Rotate_ShouldRotateCircleByGivenAngle()
+        {
+            // Arrange
+            var circle = new SitecorePreInterviewTasks.Task01.Circle(new Point(1, 1), 1);
+
+            // Act
+            circle.Rotate(Math.PI / 2);
+
+            // Assert
+            Assert.AreEqual(-1, circle.Center.X, 0.0001);
+            Assert.AreEqual(1, circle.Center.Y, 0.0001);
+        }
+
+        [TestMethod]
+        public void Rotate_ShouldRotateCircleByGivenAngle_DoublePrecision()
+        {
+            // Arrange
+            var circle = new SitecorePreInterviewTasks.Task01.Circle(new Point(1.0, 1.0), 1);
+
+            // Act
+            circle.Rotate(Math.PI / 4);
+
+            // Assert
+            Assert.AreEqual(0, circle.Center.X, 0.0001);
+            Assert.AreEqual(1.4142, circle.Center.Y, 0.0001);
+        }
+
+    }
+}
---
+++ SitecorePreInterviewTasks/Task01/Circle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitecorePreInterviewTasks.Task01
+{
+    /// <summary>
+    /// Represents a circle in 2D space.
+    /// </summary>
+    public class Circle
+    {
+        public decimal Radius { get; set; }
+        public Point Center { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Circle"/> class.
+        /// </summary>
+        /// <param name="center"></param>
+        /// <param name="radius"></param>
+        public Circle(Point center, decimal radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// Moves the circle by the given coordinates.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void Move(double x, double y)
+        {
+            Center.Move(x, y);
+        }
+
+
+        /// <summary>
+        /// Moves the circle by the given point.
+        /// </summary>
+        /// <param name="point"></param>
+        public void Move(Point point)
+        {
+            Center.Move(point);
+        }
+
+
+        /// <summary>
+        /// Rotates the circle by the given angle.
+        /// </summary>
+        /// <param name="angle"></param>
+        public void Rotate(double angle)
+        {
+            Center.Rotate(angle);
+        }
+    }
+}
