58d1ff3050c108ef10bb9bcacddd8d2855fba02d
tan0s <tanujchamil@gmail.com>
2024-08-14 20:57:09 -0700

    Added commets for the Palindrome Class

--- SitecorePreInterviewTasks/Task02/Palindrome.cs
+++ SitecorePreInterviewTasks/Task02/Palindrome.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace SitecorePreInterviewTasks.Task02
 {
+    /// <summary>
+    /// Class to check if a string is a palindrome with or without trash symbols
+    /// </summary>
     public class Palindrome
     {
         public string InputString { get; set; } = String.Empty;
@@ -20,34 +23,44 @@ namespace SitecorePreInterviewTasks.Task02
             TrashSymbolString = trashSymbolString;
             checkPalindrome();
         }
-        public void checkPalindrome()
+
+        /// <summary>
+        /// Method to check if the input string is a palindrome with or without trash symbols
+        /// </summary>
+        private void checkPalindrome()
         {
+            // Convert the input string to lower case to make the comparison case-insensitive
             string inputString = InputString.ToLower();
-
+            
             int length = inputString.Length;
-
             int left = 0;
             int right = length - 1;
 
             while (left < right)
             {
-
+                // Skip trash symbols from the left
                 while (TrashSymbolString.Contains(inputString[left]) && left < right)
                 {
+                    // if current character is trash symbol, move to the next character
                     left++;
                 }
 
+                // Skip trash symbols from the right
                 while (TrashSymbolString.Contains(inputString[right]) && left < right)
                 {
+                    // if current character is trash symbol, move to the next character
                     right--;
                 }
 
+                // Check if the non-trash characters are equal
                 if (inputString[left] != inputString[right])
                 {
+                    // if the characters are not equal, the input string is not a palindrome
                     isPalindrome = false;
                     break;
                 }
 
+                // Move to the next characters
                 left++;
                 right--;
             }
